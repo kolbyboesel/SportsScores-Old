f0b89bbbecf753ac663009e13d080ce04baa8dd4
kolbyboesel <62734896+kolbyboesel@users.noreply.github.com>
2023-04-10 20:19:42 -0500

    "combined MLB and NBA loading to one .js file"

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const options = {
 
 let data;
 
-fetch('https://odds.p.rapidapi.com/v4/sports/baseball_mlb/scores?daysFrom=1', options)
+fetch(url, options)
 	.then(response => response.json())
 	.then(response => {
 		data = response;
@@ -19,8 +19,56 @@ fetch('https://odds.p.rapidapi.com/v4/sports/baseball_mlb/scores?daysFrom=1', op
 
 console.log(data);
 
+async function getNBA() {
+	let url = 'https://odds.p.rapidapi.com/v4/sports/basketball_nba/scores?daysFrom=2';
+	try {
+		let res = await fetch(url, options);
+		return await res.json();
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function loadNBA() {
+    let allScores = await getNBA();
+    let html = '';
+    allScores.forEach(currentScore => {
+		let awayScore = 0;
+		let homeScore = 0;
+		try {
+			awayScore = currentScore.scores[1].score;
+			homeScore = currentScore.scores[0].score;
+		} catch (error) {
+			homeScore = 0;
+			awayScore = 0;
+		}
+		let compareHomeScore = homeScore;
+		let compareAwayScore = awayScore;
+
+		if(homeScore.length != 3){
+			compareHomeScore = "0" + homeScore;
+		}
+		if(awayScore.length != 3){
+			compareAwayScore = "0" + awayScore;
+		}
+		let winningTeam;
+
+		if(compareAwayScore > compareHomeScore){
+            winningTeam = currentScore.away_team;
+		}
+        else{
+            winningTeam = currentScore.home_team;
+		}
+    html += generateScoreboard(currentScore, awayScore, homeScore, winningTeam);
+
+    });
+
+    let container = document.querySelector('.containerNBA');
+    container.innerHTML = html;
+}
+
 async function getMLB() {
-	let url = 'https://odds.p.rapidapi.com/v4/sports/baseball_mlb/scores?';
+	let url = 'https://odds.p.rapidapi.com/v4/sports/baseball_mlb/scores?daysFrom=1';
 	try {
 		let res = await fetch(url, options);
 		return await res.json();
@@ -62,35 +110,34 @@ async function loadMLB() {
 		}
     html += generateScoreboard(currentScore, awayScore, homeScore, winningTeam);
 
-    });
+});
 
-    let container = document.querySelector('.container');
-    container.innerHTML = html;
+let container = document.querySelector('.containerMLB');
+container.innerHTML = html;
 }
 
 function generateScoreboard(currentScore, awayScore, homeScore, winningTeam) {
   let htmlSegment = `<div class="outer">
     <div class="scoreboard">`;
 
-    let gameStatus;
+	let gameStatus;
     if(currentScore.completed){
         gameStatus = "FINAL";
     }
     else{
         gameStatus = ""
     }
-
-    if (winningTeam === currentScore.away_team) {
-        htmlSegment += `
-        <div class="team win">
+  if (winningTeam === currentScore.away_team) {
+    htmlSegment += `
+      <div class="team win">
         <div class="team">${currentScore.away_team}</div>
         <div class="score">${awayScore}</div>
-        </div>
-        <div class="divider"><p>${gameStatus}</p></div>
-        <div class="team lose">
+      </div>
+      <div class="divider"><p>${gameStatus}</p></div>
+      <div class="team lose">
         <div class="team">${currentScore.home_team}</div>
         <div class="score">${homeScore}</div>
-        </div>`;
+      </div>`;
   } else {
     htmlSegment += `
       <div class="team lose">
@@ -110,53 +157,4 @@ function generateScoreboard(currentScore, awayScore, homeScore, winningTeam) {
 
   return htmlSegment;
 }
-
-function homeTeamWin(currentScore, awayScore, homeScore){
-    let gameStatus;
-    if(currentScore.completed){
-        gameStatus = "FINAL";
-    }
-    else{
-        gameStatus = ""
-    }
-	let htmlSegment =`<div class="outer">
-			<div class="scoreboard">
-        		<div class="team lose">
-            		<div class="team">${currentScore.away_team}</div>
-            		<div class="score">${awayScore}</div>
-          		</div>
-          		<div class="divider"><p>${gameStatus}</p></div>
-          		<div class="team win">
-            		<div class="team">${currentScore.home_team}</div>
-            		<div class="score">${homeScore}</div>
-       			</div>
-			</div>
-		</div>`;
-        return htmlSegment;
-}
-
-function awayTeamWin(currentScore, awayScore, homeScore){
-    if(currentScore.completed){
-        gameStatus = "FINAL";
-    }
-    else{
-        gameStatus = ""
-    }
-	let htmlSegment =`<div class="outer">
-			<div class="scoreboard">
-        		<div class="team win">
-            		<div class="team">${currentScore.away_team}</div>
-            		<div class="score">${awayScore}</div>
-          		</div>
-          		<div class="divider"><p>${gameStatus}</p></div>
-          		<div class="team lose">
-            		<div class="team">${currentScore.home_team}</div>
-            		<div class="score">${homeScore}</div>
-       			</div>
-			</div>
-		</div>`;
-        return htmlSegment;
-}
-
-loadMLB();
 
