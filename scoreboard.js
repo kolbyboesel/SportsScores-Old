463ced5421c82858cd0289392f48d5f1b5034d6e
kboesel <kboesel@gmail.com>
2023-04-11 14:15:33 -0500

    White space formatting

--- scoreboard.js
+++ scoreboard.js
@@ -2,8 +2,8 @@
 
 function clear(elementID)
 {
-    let container = document.querySelector(elementID);
-    container.innerHTML = "";
+  let container = document.querySelector(elementID);
+  container.innerHTML = "";
 }
 
 const options = {
@@ -15,12 +15,12 @@ const options = {
 };
 
 async function getData(url){
-	try {
-		let res = await fetch(url, options);
-		return await res.json();
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    let res = await fetch(url, options);
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function showNBAScores() {
@@ -32,28 +32,28 @@ async function showMLBScores() {
 }
 
 async function buildScoreboard(allScores, containerName) {
-	clear('.' + containerName);
+  clear('.' + containerName);
 
-    let html = '';
-    allScores.forEach(currentScore => {
-      let awayScore = 0;
-      let homeScore = 0;
+  let html = '';
+  allScores.forEach(currentScore => {
+    let awayScore = 0;
+    let homeScore = 0;
 
-      try {
-        let awayScoreRaw = currentScore.scores[1].score;
-        let homeScoreRaw = currentScore.scores[0].score;
+    try {
+      let awayScoreRaw = currentScore.scores[1].score;
+      let homeScoreRaw = currentScore.scores[0].score;
 
-        awayScoreRaw === null ? awayScore = 0 : awayScore = Number(awayScoreRaw);
-        homeScoreRaw === null ? homeScore = 0 : homeScore = Number(homeScoreRaw);
-      } catch (error) {
-        homeScore = 0;
-        awayScore = 0;
-      }
+      awayScoreRaw === null ? awayScore = 0 : awayScore = Number(awayScoreRaw);
+      homeScoreRaw === null ? homeScore = 0 : homeScore = Number(homeScoreRaw);
+    } catch (error) {
+      homeScore = 0;
+      awayScore = 0;
+    }
 
-      let winningTeam = awayScore < homeScore ? currentScore.home_team : currentScore.away_team;
-      let completedDate = formatDate(currentScore.commence_time);
+    let winningTeam = awayScore < homeScore ? currentScore.home_team : currentScore.away_team;
+    let completedDate = formatDate(currentScore.commence_time);
 
-      html += generateScoreboard(currentScore, awayScore, homeScore, winningTeam, completedDate);
+    html += generateScoreboard(currentScore, awayScore, homeScore, winningTeam, completedDate);
   });
 
   let container = document.querySelector('.' + containerName);
@@ -61,10 +61,10 @@ async function buildScoreboard(allScores, containerName) {
 }
 
 function formatDate(rawDate){
-	let dateTimeValue = Date.parse(rawDate);
-	let dateRaw = new Date(dateTimeValue);
+  let dateTimeValue = Date.parse(rawDate);
+  let dateRaw = new Date(dateTimeValue);
 
-	return dateRaw.toLocaleDateString() + " Start Time: " + dateRaw.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+  return dateRaw.toLocaleDateString() + " Start Time: " + dateRaw.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
 }
 
 function generateScoreboard(currentScore, awayScore, homeScore, winningTeam, dateTimeValue) {
